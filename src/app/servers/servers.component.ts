d77d383299bcf7aba03c833c5110067a89a57754
Hasan_Ali <hasan.aly.dev@gmail.com>
2024-05-01 18:59:55 +0300

    first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  displayDetails = false;
+  displayStatus = 'Display Details';
+  displayLog = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onDisplayClicked() {
+    this.displayDetails = !this.displayDetails;
+    this.displayStatus =
+      this.displayDetails === false ? 'Display Details' : 'Hide Details';
+    this.displayLog.push(
+      this.displayDetails === false
+        ? ' Details hidden'
+        :' Details displayed'
+    );
+  }
+}
